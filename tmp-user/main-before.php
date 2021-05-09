6bbe6c6ad0e4eb55fb06b6b2cab08b76d40cea2c
muyooo <muyooo.info@gmail.com>
2021-05-09 19:10:10 +0900

    Clean code for category article analyze

--- tmp-user/main-before.php
+++ tmp-user/main-before.php
@@ -1,6 +1,7 @@
 <?php
 echo '<section class="add-contents wrap">
-  <ul class="analyze-cat">';
+  <h2 class="analyze-cat-title">コンテンツ一覧</h2>
+  <ul class="analyze-cat analyze-cat--hide">';
     // カテゴリーの取得
     $categories = get_categories();
     $categoryCounts = [];
@@ -14,45 +15,42 @@ echo '<section class="add-contents wrap">
         ]
       );
       $categoryURL = get_site_url() . '/category/' . $categoryDetail->slug . '/';
-      $categoryClass = 'analyze-cat-' . $categoryDetail->slug . '-ratio';
+      $categoryClass = 'analyze-cat-ratio';
+      $categoryUniqueClass = $categoryClass . '--' . $categoryDetail->slug;
       $moveStopClass = 'analyze-cat-stop';
       echo '
         <li class="analyze-cat-list">
-          <a href="' . $categoryURL . '" class="' . $categoryClass . ' ' . $moveStopClass .'">' . $categoryDetail->name . '</a>
+          <a
+            href="' . $categoryURL . '" 
+            class="' . 
+              $categoryClass . ' ' . 
+              $categoryUniqueClass . ' ' . 
+              $moveStopClass .'">' . 
+            $categoryDetail->name . 
+          '</a>
         </li>
       ';
     }
     // カテゴリー比率の計算
     $categoryArticleCountSum = array_sum($categoryCounts['count']);
     $categoryRatioValue = round(100 / $categoryArticleCountSum, 1);
-    echo '<style>';
+    echo '<style type="text/css">';
       $i = 0;
       for($i; $i < count($categoryCounts['slug']); $i++) {
         // 比率の付与
         $targetRatio = $categoryRatioValue * $categoryCounts['count'][$i];
-        $targetMoveClassBefore = '.analyze-cat-' . $categoryCounts['slug'][$i] . '-ratio::before';
+        $targetMoveClassBefore = '.' . $categoryClass . '--' . $categoryCounts['slug'][$i] . '::before';
         $targetDeg = 0;
         if ($targetRatio <= 50) {
           $targetDeg = 360 * $targetRatio / 100;
-          echo $targetMoveClassBefore . '{transform: rotate(' . $targetDeg . 'deg);transition: .8s;}'; 
+          echo $targetMoveClassBefore . '{transform: rotate(' . $targetDeg . 'deg);}'; 
         } else {
           $targetDeg = 360 * ($targetRatio - 50) / 100;
-          echo $targetMoveClassBefore . '{transform: rotate(' . $targetDeg . 'deg);background-color: #7a99cf;transition: .8s;}'; 
+          echo $targetMoveClassBefore . '{transform: rotate(' . $targetDeg . 'deg);background-color: #26499d;}'; 
         }
-        $targetClassAfter = '.analyze-cat-' . $categoryCounts['slug'][$i] . '-ratio::after';
+        $targetClassAfter = '.' . $categoryClass . '--' . $categoryCounts['slug'][$i] . '::after';
         echo $targetClassAfter . '{content:"' . $targetRatio .'%";}';
       }
-    echo ".analyze-cat-stop::before {transform: rotate(0deg);}</style>
-    <script type='text/javascript'>
-      'use strict';
-      setTimeout(() => {
-        const analyzeData = document.querySelectorAll('.analyze-cat-list a');
-        console.log(analyzeData);
-        let i = 0;
-        for(i; i < analyzeData.length; i++) {
-          analyzeData[i].classList.remove('analyze-cat-stop');
-        }
-      }, 200);
-    </script>
+    echo '.analyze-cat-stop::before {transform: rotate(0deg);}</style>
   </ul>
-</section>";
+</section>';
