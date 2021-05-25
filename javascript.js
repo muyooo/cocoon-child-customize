54b2e53386560c85fd08883867c7e8bcdc8acb41
muyooo <muyooo.info@gmail.com>
2021-05-25 20:28:33 +0900

    Show analyze list when only exist add-contents

--- javascript.js
+++ javascript.js
@@ -4,16 +4,19 @@
 'use strict';
 
 window.addEventListener('load', () => {
-  setTimeout(() => {
-    const analyzeList = document.querySelector('.analyze-cat');
-    analyzeList.classList.remove('analyze-cat--hide');
-  }, 100);
-  
-  setTimeout(() => {
-    const analyzeData = document.querySelectorAll('.analyze-cat-list a');
-    let i = 0;
-    for(i; i < analyzeData.length; i++) {
-      analyzeData[i].classList.remove('analyze-cat-stop');
-    }
-  }, 200);
+  const addContents = document.querySelector('.add-contents');
+  if (addContents != null) {
+    setTimeout(() => {
+      const analyzeList = document.querySelector('.analyze-cat');
+      analyzeList.classList.remove('analyze-cat--hide');
+    }, 50);
+
+    setTimeout(() => {
+      const analyzeData = document.querySelectorAll('.analyze-cat-list a');
+      let i = 0;
+      for(i; i < analyzeData.length; i++) {
+        analyzeData[i].classList.remove('analyze-cat-stop');
+      }
+    }, 200);
+  }
 });
