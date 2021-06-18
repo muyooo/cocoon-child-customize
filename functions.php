99204e074b068b0a785c7ff304928ad47cab9990
muyooo <muyooo.info@gmail.com>
2021-06-18 17:39:05 +0900

    Clean code

--- functions.php
+++ functions.php
@@ -5,27 +5,34 @@ if ( !defined( 'ABSPATH' ) ) exit;
 add_editor_style();
 
 //以下に子テーマ用の関数を書く
-function get_current_link() {
+function get_current_link()
+{
   return (is_ssl() ? 'https' : 'http') . '://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
 }
-function get_current_slug() {
+function get_current_slug()
+{
   $currentLink = get_current_link();
   $currentCatSlug = rtrim($currentLink, '/');
   return substr($currentCatSlug, strrpos($currentCatSlug, '/') + 1);
 }
-function get_icon_url($slug) {
+function get_icon_url($slug)
+{
   return esc_attr( get_site_url() . '/wp-content/uploads/icon_' . $slug . '.png');
 }
-function get_category_url($slug) {
+function get_category_url($slug)
+{
   return esc_attr( get_site_url() . '/category/' . $slug . '/' );
 }
-function get_analyze_move_stop_class() {
+function get_analyze_move_stop_class()
+{
   $moveStopClass = 'analyze-cat-stop';
   return esc_attr( $moveStopClass );
 }
-function get_analyze_cat_ratio_class() {
+function get_analyze_cat_ratio_class()
+{
   return esc_attr( 'analyze-cat-ratio' );
 }
-function get_analyze_cat_ratio_unique_class($slug) {
+function get_analyze_cat_ratio_unique_class($slug)
+{
   return rtrim( get_analyze_cat_ratio_class() ) . '--' . $slug;
 }
