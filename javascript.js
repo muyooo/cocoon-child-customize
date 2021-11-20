5b6f83da6ad9802da53a9b57db1a5905956dac0e
muyooo <muyooo.info@gmail.com>
2021-11-20 15:26:40 +0900

    Change const and let to var

--- javascript.js
+++ javascript.js
@@ -3,17 +3,17 @@
 //JavaScriptやjQueryで親テーマのjavascript.jsに加えて関数を記入したい時に使用します。
 'use strict';
 
-  const addContents = document.querySelector('.add-contents');
 window.addEventListener('DOMContentLoaded', () => {
+  var addContents = document.querySelector('.add-contents');
   if (addContents != null) {
     setTimeout(() => {
-      const analyzeList = document.querySelector('.analyze-cat');
+      var analyzeList = document.querySelector('.analyze-cat');
       analyzeList.classList.remove('analyze-cat--hide');
     }, 50);
 
     setTimeout(() => {
-      const analyzeData = document.querySelectorAll('.analyze-cat-list a');
-      let i = 0;
+      var analyzeData = document.querySelectorAll('.analyze-cat-list a');
+      var i = 0;
       for(i; i < analyzeData.length; i++) {
         analyzeData[i].classList.remove('analyze-cat-stop');
       }
