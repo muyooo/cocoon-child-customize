cf658964146e2cb94a5549635c4c6e1c81fd6dce
muyooo <muyooo.info@gmail.com>
2024-10-22 17:56:04 +0900

    Remove 2nd Row Main Content  and Sponsor Link for top

--- tmp-user/main-before.php
+++ tmp-user/main-before.php
@@ -41,58 +41,9 @@
       <?php endif; ?>
     <?php endforeach; ?>
     </ul>
-    <h2 class="analyze-cat-title analyze-cat-title--topline">好きなこと・やったこと</h2>
-    <ul class="analyze-cat analyze-cat--hide">
-    <?php foreach( $categories as $category ) : ?>
-      <?php if ($category->parent === 22): ?>
-        <?php
-          // カテゴリー情報の取得とリンクの作成
-            $categoryDetail = get_category($category->term_id);
-            $categoryCounts = array_merge_recursive(
-              $categoryCounts,
-              [
-                'count' => $categoryDetail->count,
-                'slug' => $categoryDetail->slug
-              ]
-            );
-        ?>
-        <li class="analyze-cat-list analyze-cat-list-<?= esc_attr( $categoryDetail->slug ) ?>">
-          <a
-            href="<?= get_page_url( $categoryDetail->slug ); ?>" 
-            class="
-              <?= get_analyze_cat_ratio_class(); ?> 
-              <?= get_analyze_cat_ratio_unique_class( $categoryDetail->slug ); ?> 
-              <?= get_analyze_move_stop_class(); ?>">
-            <div class="analyze-cat-image">
-              <img src="<?= get_icon_url( $categoryDetail->slug ); ?>" class="analyze-cat-icon">
-            </div>
-            <span class="analyze-cat-headline analyze-cat-headline--<?= esc_attr( $categoryDetail->slug );?>">
-              <?= esc_attr( $categoryDetail->name ); ?><br>
-            </span>
-            <p class="analyze-cat-desc">
-              <?= get_category_description($categoryDetail->term_id); ?>
-            </p>
-            <span class="fa fa-chevron-down analyze-cat-arrow" aria-hidden="true"></span>
-          </a>
-        </li>
-      <?php endif; ?>
-    <?php endforeach; ?>
-    </ul>
     <div class="wp-block-button add-contents-button">
       <a class="wp-block-button__link wp-element-button" href="https://muyooo.com/about/"><img src="https://muyooo.com/wp-content/uploads/icon_muyooo.png" alt="著者アイコン">運営者のプロフィールはこちら</a>
     </div>
-    <div class="ad-label" itemprop="name" data-nosnippet="">スポンサーリンク</div>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <!-- muyooo.com ad -->
-    <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-8959537944225967"
-        data-ad-slot="3743194221"
-        data-ad-format="horizontal"
-        data-full-width-responsive="true"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
     <style type="text/css">
       <?php
         // カテゴリー比率の計算と設定
